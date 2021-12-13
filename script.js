94a8aa567ccc8bec019797ab27b64b70b0a05a56
Matan <matanbitton2@gmail.com>
2021-12-13 23:31:04 +0200

    added reset function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const body = document.body;
 const container = document.getElementById("container")
+document.getElementById("reset").onclick = resetSquares;
 
 
 body.append(container);
 
 
-
+let squaresArray = [];
 
 function createGrid(numOfSquares) {
+    if (numOfSquares > 100) {
+        alert("Numbers above 100 are not allowed.")
+    }else {
     //the square sizes calculated minus 2 because of the gap between squares.
      sizeOfSquares = (502/numOfSquares)-2;
      
@@ -18,19 +22,25 @@ function createGrid(numOfSquares) {
         `
         width:${sizeOfSquares}px;
         height:${sizeOfSquares}px;
+        background-color: rgb(177, 6, 6);
         `;  
-     
+         squaresArray.push(squareItem);
         squareItem.addEventListener("mouseover", () => {
-            squareItem.style.backgroundColor = "black";
+            squareItem.style.backgroundColor = "yellow";
         });    
         
         container.append(squareItem);
     }
+    }
 }
 
-   
-    
+function resetSquares() {
+     
+    for (let i = 0; i< squaresArray.length; i++) {
+         squaresArray[i].style.backgroundColor= "rgb(177, 6, 6)";
+    }
+
+};
 
 
-
-createGrid(16);
+createGrid(64);
